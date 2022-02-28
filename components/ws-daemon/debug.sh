f3d2c4a6b46e24ad0c3c7ef895b2a3c1f0215433
Thomas Schubart <thomas@gitpod.io>
2022-02-28 17:18:15 +0000

    Adapt debug script

--- components/ws-daemon/debug.sh
+++ components/ws-daemon/debug.sh
@@ -9,10 +9,10 @@
 # BEWARE: the properly built version of ws-daemon may behave differently.
 
 docker ps &> /dev/null || (echo "You need a working Docker daemon. Maybe set DOCKER_HOST?"; exit 1)
-gcloud auth list | grep typefox &>/dev/null || (echo "Login using 'gcloud auth login' for the docker push to work"; exit 1)
+#gcloud auth list | grep thomas &>/dev/null || (echo "Login using 'gcloud auth login' for the docker push to work"; exit 1)
 
-leeway build .:docker -Dversion=dev
-devImage=eu.gcr.io/gitpod-dev/ws-daemon:dev
+leeway build .:docker -Dversion=fo-limit-cg-2 -DimageRepoBase=eu.gcr.io/gitpod-core-dev/dev
+devImage=eu.gcr.io/gitpod-core-dev/dev/ws-daemon:fo-limit-cg-2
 
 kubectl patch daemonset ws-daemon --patch '{"spec": {"template": {"spec": {"containers": [{"name": "ws-daemon","image": "'$devImage'"}]}}}}'
 kubectl get pods --no-headers -o=custom-columns=:metadata.name | grep ws-daemon | xargs kubectl delete pod
